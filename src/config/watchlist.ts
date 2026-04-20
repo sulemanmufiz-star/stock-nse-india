7da78420b67f73229c1da05a1932534d3599b543
sulemanmufiz-star <sulemanmufiz@gmail.com>
2026-04-20 23:04:57 +0530

    Update watchlist.ts

--- src/config/watchlist.ts
+++ src/config/watchlist.ts
@@ -1,16 +1,19 @@
-module.exports = {
+const watchlist = {
   sectors: {
+    "NIFTY IT": [
+      "TCS",
+      "INFY",
+      "WIPRO",
+      "HCLTECH",
+      "TECHM"
+    ],
+
     "NIFTY AUTO": [
       "TATAMOTORS",
       "MARUTI",
       "M&M",
       "BAJAJ-AUTO",
-      "EICHERMOT",
-      "HEROMOTOCO",
-      "TVSMOTOR",
-      "ASHOKLEY",
-      "BALKRISIND",
-      "BOSCHLTD"
+      "EICHERMOT"
     ],
 
     "NIFTY BANK": [
@@ -18,155 +21,9 @@ module.exports = {
       "ICICIBANK",
       "SBIN",
       "AXISBANK",
-      "KOTAKBANK",
-      "INDUSINDBK",
-      "BANKBARODA",
-      "PNB",
-      "AUBANK",
-      "FEDERALBNK"
-    ],
-
-    "NIFTY FIN SERVICE": [
-      "BAJFINANCE",
-      "BAJAJFINSV",
-      "SBILIFE",
-      "HDFCLIFE",
-      "ICICIPRULI",
-      "CHOLAFIN",
-      "SHRIRAMFIN",
-      "RECLTD",
-      "PFC",
-      "SBICARD"
-    ],
-
-    "NIFTY FMCG": [
-      "HINDUNILVR",
-      "ITC",
-      "NESTLEIND",
-      "BRITANNIA",
-      "DABUR",
-      "GODREJCP",
-      "TATACONSUM",
-      "COLPAL",
-      "MARICO",
-      "UBL"
-    ],
-
-    "NIFTY IT": [
-      "TCS",
-      "INFY",
-      "WIPRO",
-      "HCLTECH",
-      "TECHM",
-      "LTIM",
-      "PERSISTENT",
-      "MPHASIS",
-      "COFORGE",
-      "OFSS"
-    ],
-
-    "NIFTY METAL": [
-      "TATASTEEL",
-      "JSWSTEEL",
-      "HINDALCO",
-      "VEDL",
-      "SAIL",
-      "JINDALSTEL",
-      "NMDC",
-      "NATIONALUM",
-      "HINDZINC",
-      "APLAPOLLO"
-    ],
-
-    "NIFTY PHARMA": [
-      "SUNPHARMA",
-      "DIVISLAB",
-      "DRREDDY",
-      "CIPLA",
-      "LUPIN",
-      "AUROPHARMA",
-      "TORNTPHARM",
-      "ALKEM",
-      "BIOCON",
-      "ZYDUSLIFE"
-    ],
-
-    "NIFTY PSU BANK": [
-      "SBIN",
-      "BANKBARODA",
-      "PNB",
-      "CANBK",
-      "UNIONBANK",
-      "INDIANB",
-      "BANKINDIA",
-      "CENTRALBK",
-      "UCOBANK",
-      "MAHABANK"
-    ],
-
-    "NIFTY REALTY": [
-      "DLF",
-      "GODREJPROP",
-      "OBEROIRLTY",
-      "PRESTIGE",
-      "PHOENIXLTD",
-      "BRIGADE",
-      "SOBHA",
-      "SUNTECK",
-      "LODHA",
-      "MAHLIFE"
-    ],
-
-    "NIFTY ENERGY": [
-      "RELIANCE",
-      "ONGC",
-      "NTPC",
-      "POWERGRID",
-      "BPCL",
-      "IOC",
-      "GAIL",
-      "ADANIGREEN",
-      "ADANIPOWER",
-      "TATAPOWER"
-    ],
-
-    "NIFTY OIL & GAS": [
-      "RELIANCE",
-      "ONGC",
-      "BPCL",
-      "IOC",
-      "GAIL",
-      "HINDPETRO",
-      "OIL",
-      "PETRONET",
-      "GUJGASLTD",
-      "ATGL"
-    ],
-
-    "NIFTY CONSUMER DURABLES": [
-      "TITAN",
-      "VOLTAS",
-      "WHIRLPOOL",
-      "HAVELLS",
-      "DIXON",
-      "CROMPTON",
-      "BLUESTARCO",
-      "RAJESHEXPO",
-      "KAJARIACER",
-      "AMBER"
-    ],
-
-    "NIFTY HEALTHCARE": [
-      "SUNPHARMA",
-      "DIVISLAB",
-      "DRREDDY",
-      "CIPLA",
-      "APOLLOHOSP",
-      "MAXHEALTH",
-      "FORTIS",
-      "LALPATHLAB",
-      "METROPOLIS",
-      "SYNGENE"
+      "KOTAKBANK"
     ]
   }
 };
+
+export default watchlist;
